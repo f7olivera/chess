e57f6c17e92de7facf7fbe9a30eccb9205fcecd0
f7olivera <f7olivera@gmail.com>
2022-05-25 21:12:12 -0300

    Fix Timer

--- frontend/static/js/components/Timer.jsx
+++ frontend/static/js/components/Timer.jsx
@@ -5,30 +5,29 @@ import AdjustingInterval from "../misc/timer.js";
 export default function Timer({ time, setTime, running }) {
   const [stop, setStop] = React.useState(false);
   const [timeFormat, setTimeFormat] = React.useState();
-
-  const asd = () => {
+  const update = () => {
     const secondsLeft = ticker.time - (Date.now() - ticker.start_ts) / 1000;
-    if (secondsLeft < 0) {
-      const newTicker = {...ticker};
-      newTicker.stop();
-      setTicker(newTicker);
-      setStop(true);
-      setTime(0);
-    } else {
-      setTime(secondsLeft);
-    }
+    setTime(secondsLeft < 0 ? 0.000000001 : secondsLeft)
   }
-  const [ticker, setTicker] = React.useState(new AdjustingInterval(asd, 100, () => {}));
+  const [ticker, setTicker] = React.useState(new AdjustingInterval(update, 100, () => {}));
+
   if (running) {!stop && !ticker.running && ticker.start()}
   if (!running) {ticker.stop()}
   stop && ticker.stop()
 
   React.useEffect(() => {
+    if (time === 0) {
+      const newTicker = {...ticker};
+      newTicker.stop();
+      setStop(true);
+      setTicker(newTicker);
+    }
     const secondsLeft = time;
     const deciseconds = Math.floor((secondsLeft % 1) * 10);
     const date = new Date(0);
     date.setSeconds(secondsLeft < 0 ? 0 : secondsLeft); // specify value for SECONDS here
-    const timeString = date.toISOString().substr(secondsLeft >= 3600 ? 11 : 14, secondsLeft >= 3600 ? 8 : 5) + (0 < secondsLeft && secondsLeft < 10 ? '.' + deciseconds.toString() : '');
+    console.log(secondsLeft)
+    const timeString = date.toISOString().substr(secondsLeft >= 3600 ? 11 : 14, secondsLeft >= 3600 ? 8 : 5) + (0.000000001 < secondsLeft && secondsLeft < 10 ? '.' + deciseconds.toString() : '');
     setTimeFormat(timeString);
   }, [time])
 
