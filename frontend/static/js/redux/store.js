cf2605442b2fd6aebbc7bbe18530e4d3b6eae86e
f7olivera <f7olivera@gmail.com>
2022-09-02 16:47:26 -0300

    Remove unused Redux slice

--- frontend/static/js/redux/store.js
+++ frontend/static/js/redux/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import chessReducer from "./chessSlice";
 import gameReducer from "./gameSlice.js";
 
 export default configureStore({
   reducer: {
-    // chess: chessReducer,
     game: gameReducer,
   },
 })
